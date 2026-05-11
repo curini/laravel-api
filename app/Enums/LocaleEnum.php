f2191f51ae71d4d2d4c3daa2e6e12888a44089ae
LAPTOP-J9U9GBB8\curin <curini>
2026-05-11 17:40:19 +0200

    add enums

---
+++ app/Enums/LocaleEnum.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Enums;
+
+enum LocaleEnum: string
+{
+    case FR = 'fr';
+}
---
+++ app/Enums/StatesEnum.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace App\Enums;
+
+enum StatesEnum: string
+{
+    case SALE = 'vente';
+    case LOCATION = 'location';
+}
