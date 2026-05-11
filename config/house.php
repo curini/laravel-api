c85fa1baa5daa2307f75857de24dce0aba930385
LAPTOP-J9U9GBB8\curin <curini>
2026-05-11 17:41:35 +0200

    add new config file for house api

---
+++ config/house.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'url' => env('URL_HOUSES_API', 'www.iadfrance.fr'),
+
+    'properties_path' => 'api/properties',
+
+    'from_slug_path' => 'api/locations/from-slugs',
+];
