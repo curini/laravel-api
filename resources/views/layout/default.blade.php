36374c9e47e8d22ea5b2cec50b8caaee9e68d5b6
LAPTOP-J9U9GBB8\curin <curini>
2026-06-01 17:33:21 +0200

    update route and default layout

--- resources/views/layout/default.blade.php
+++ resources/views/layout/default.blade.php
@@ -10,6 +10,7 @@
 </head>
 
 <body>
+    @include('components.nav')
     @yield('content')
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,18 @@ Route::get('/', function () {
     return 'api laravel';
 })->name('home');
 
-Route::prefix('formfile')->name('form.file.')->group(function () {
+Route::prefix('formfile')->name('form.file.')->middleware('auth:web')->group(function () {
     Route::get('create', [FormFileController::class, 'create'])->name('create');
     Route::post('store', [FormFileController::class, 'store'])->name('store');
 });
+
+
+Route::get(RoutePath::for('login', '/login'), function () {
+    return view('login');
+})
+    ->middleware(['guest:' . config('fortify.guard')])
+    ->name('login');
+
 Route::get(RoutePath::for('password.reset', '/reset-password/{token}'), [NewPasswordController::class, 'create'])
     ->middleware(['guest:' . config('fortify.guard')])
     ->name('password.reset');
