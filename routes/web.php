1bd4136f91398052799e6e5ee71df10282b6ec93
LAPTOP-J9U9GBB8\curin <curini>
2026-06-01 17:31:41 +0200

    add fortify and default settings with disable views

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FormFileController;
+use Laravel\Fortify\RoutePath;
+use Laravel\Fortify\Http\Controllers\NewPasswordController;
 
 Route::get('/', function () {
     return 'api laravel';
@@ -11,3 +13,6 @@ Route::prefix('formfile')->name('form.file.')->group(function () {
     Route::get('create', [FormFileController::class, 'create'])->name('create');
     Route::post('store', [FormFileController::class, 'store'])->name('store');
 });
+Route::get(RoutePath::for('password.reset', '/reset-password/{token}'), [NewPasswordController::class, 'create'])
+    ->middleware(['guest:' . config('fortify.guard')])
+    ->name('password.reset');
